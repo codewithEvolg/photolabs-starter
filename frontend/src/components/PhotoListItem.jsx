0beda88fe433446d7af33a7a71d6f3f412909f7e
Oluwagbenga Ogundare <oluwagbengaogundare@gmail.com>
2023-08-25 03:45:19 -0400

    displaying multiple photoitems

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,15 +3,15 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 
 const PhotoListItem = (props) => {
-  const {photoListItem} = props;
+  const {imageSource, profile, username, city, country} = props;
   /* Insert React */
   return (
     <div className='photo-list__item'>
-      <img className='photo-list__image' src={photoListItem.imageSource} alt='sample image'/>
-      <img className='photo-list__user-profile' src={photoListItem.profile} alt='sample image'/>
-      <h4 className='photo-list__user-info'>{photoListItem.username}</h4>
+      <img className='photo-list__image' src={imageSource} alt='sample image'/>
+      <img className='photo-list__user-profile' src={profile} alt='sample image'/>
+      <h4 className='photo-list__user-info'>{username}</h4>
       <p className='photo-list__user-location'>
-        {photoListItem.location.city}, {photoListItem.location.country}
+        {city}, {country}
       </p>
     </div>
   );
