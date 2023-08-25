7bc4c34472b22767f0c2a0ea9b7970eda0b8b5b1
Oluwagbenga Ogundare <oluwagbengaogundare@gmail.com>
2023-08-25 03:12:29 -0400

    building a photolistitem in isolation

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,20 +2,19 @@ import React from "react";
 
 import "../styles/PhotoListItem.scss";
 
-
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const PhotoListItem = () => {
+const PhotoListItem = (props) => {
+  const {photoListItem} = props;
   /* Insert React */
+  return (
+    <div className='photo-list__item'>
+      <img className='photo-list__image' src={photoListItem.imageSource} alt='sample image'/>
+      <img className='photo-list__user-profile' src={photoListItem.profile} alt='sample image'/>
+      <h4 className='photo-list__user-info'>{photoListItem.username}</h4>
+      <p className='photo-list__user-location'>
+        {photoListItem.location.city}, {photoListItem.location.country}
+      </p>
+    </div>
+  );
 };
 
 export default PhotoListItem;
