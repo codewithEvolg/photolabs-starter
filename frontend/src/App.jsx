87574feb684b5d28638bda5ac0ca9d5ac939889d
Oluwagbenga Ogundare <oluwagbengaogundare@gmail.com>
2023-09-01 18:27:20 -0400

    Create custom hook and import into App.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,58 +1,26 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.scss';
 import HomeRoute from 'routes/HomeRoute';
-import photos from 'mocks/photos';
-import topics from 'mocks/topics';
 import PhotoDetailsModal from 'routes/PhotoDetailsModal';
+import useApplicationData from 'hooks/useApplicationData';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
-  const [state, setState] = useState({
-    favourites : [],
-    isDisplayModal : false,
-    selectedPhotoId : 0
-  });
-  
-  const getSelectedPhotoInfo = () => {
-    const foundPhoto = photos.find(photo => photo.id === state.selectedPhotoId); //find selected photo object with PhotoId
-    return foundPhoto;
-  };
-
-  const isFavorited = (id) => {
-    return state.favourites.includes(id);
-  };
-
-  const toggleModalSelect = () => {
-    const newFavouritesArray = isFavorited(state.selectedPhotoId)
-      ? state.favourites.filter(item => item !== state.selectedPhotoId)
-      : [...state.favourites, state.selectedPhotoId];
-
-    setState({...state, favourites: newFavouritesArray});
-  };
-
-  const handleCloseClick = () => {
-    setState({...state, isDisplayModal: false, selectedPhotoId : 0});
-  };
-
-  const handleDisplayModal = (id) =>{
-    setState({...state, isDisplayModal: true, selectedPhotoId : id});
-  };
-
-  const toggleFavourite = (id) => {
-    const newFavouritesArray = state.favourites.includes(id)
-      ? state.favourites.filter(item => item !== id)
-      : [...state.favourites, id];
-
-    setState({...state, favourites: newFavouritesArray});
-  };
-
-  
+  const {
+    state,
+    getSelectedPhotoInfo,
+    isFavorited,
+    toggleModalSelect,
+    handleCloseClick,
+    handleDisplayModal,
+    toggleFavourite
+  } = useApplicationData();
 
   return (
     <div className="App">
       <HomeRoute
-        photos = {photos}
-        topics = {topics}
+        photos = {state.photos}
+        topics = {state.topics}
         favourites = {state.favourites}
         handleDisplayModal = {handleDisplayModal}
         toggleFavourite ={toggleFavourite}
--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,19 +1,58 @@
-// const { useState } = require("react");
+import { useState } from "react";
+import photoData from 'mocks/photos';
+import topicsData from 'mocks/topics';
 
-// const useApplicationData = () => {
-//   const [state, setState] = useState({
-//     favourites : [],
-//     isDisplayModal : false,
-//     selectedPhotoId : 0
-//   });
-
-//   const toggleModalSelect = () => {
-//     const newFavouritesArray = isFavorited
-//       ? state.favourites.filter(item => item !== state.selectedPhotoId)
-//       : [...state.favourites, state.selectedPhotoId];
-
-//     setState({...state, favourites: newFavouritesArray});
-//   };
-
+const useApplicationData = () => {
+  const [state, setState] = useState({
+    favourites : [],
+    isDisplayModal : false,
+    selectedPhotoId : 0,
+    photos : photoData,
+    topics : topicsData
+  });
   
-// };
+  const getSelectedPhotoInfo = () => {
+    const foundPhoto = state.photos.find(photo => photo.id === state.selectedPhotoId); //find selected photo object with PhotoId
+    return foundPhoto;
+  };
+
+  const isFavorited = (id) => {
+    return state.favourites.includes(id);
+  };
+
+  const toggleModalSelect = () => {
+    const newFavouritesArray = isFavorited(state.selectedPhotoId)
+      ? state.favourites.filter(item => item !== state.selectedPhotoId)
+      : [...state.favourites, state.selectedPhotoId];
+
+    setState({...state, favourites: newFavouritesArray});
+  };
+
+  const handleCloseClick = () => {
+    setState({...state, isDisplayModal: false, selectedPhotoId : 0});
+  };
+
+  const handleDisplayModal = (id) =>{
+    setState({...state, isDisplayModal: true, selectedPhotoId : id});
+  };
+
+  const toggleFavourite = (id) => {
+    const newFavouritesArray = state.favourites.includes(id)
+      ? state.favourites.filter(item => item !== id)
+      : [...state.favourites, id];
+
+    setState({...state, favourites: newFavouritesArray});
+  };
+
+  return {
+    state,
+    getSelectedPhotoInfo,
+    isFavorited,
+    toggleModalSelect,
+    handleCloseClick,
+    handleDisplayModal,
+    toggleFavourite
+  };
+};
+
+export default useApplicationData;
